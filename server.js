7228f22397ef5da3a8121a5e062386790b9ec1fe
Eren Sulutas <erensulu11@gmail.com>
2019-12-19 10:50:09 -0500

    socket fix

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 var request = require('request');
 var app = require('express')();
 
-/*(const PORT = 'process.env.PORT || 3000';
-const INDEX = '/index.html';
-const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));*/
-
 var http = require('http').createServer(app);
-var io = SocketIO(server);
 var io = require('socket.io')(http);
 var fs = require('fs');
 
